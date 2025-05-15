be489e0a006479a6144ee0182061262d8e44ae01
Marc Mendoza <777marc@gmail.com>
2025-05-15 10:16:05 -0400

    Initial commit

---
+++ test/apig-jwt.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApigJwt from '../lib/apig-jwt-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/apig-jwt-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApigJwt.ApigJwtStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
